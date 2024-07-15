e4c626408bcbf4a3c3694f7bfdb09fd53180bbe0
Giovane Souza <developergiovanesouza@gmail.com>
2024-07-15 13:03:00 -0300

    feat: crud do sistema de cadastro

---
+++ SistemaCadastro/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace SistemaCadastro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtData = new System.Windows.Forms.DateTimePicker();
+            this.comboEC = new System.Windows.Forms.ComboBox();
+            this.txtTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.checkCasa = new System.Windows.Forms.CheckBox();
+            this.checkVeiculo = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioM = new System.Windows.Forms.RadioButton();
+            this.radioF = new System.Windows.Forms.RadioButton();
+            this.radioO = new System.Windows.Forms.RadioButton();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // txtNome
+            // 
+            this.txtNome.AcceptsReturn = true;
+            resources.ApplyResources(this.txtNome, "txtNome");
+            this.txtNome.Name = "txtNome";
+            // 
+            // txtData
+            // 
+            resources.ApplyResources(this.txtData, "txtData");
+            this.txtData.Name = "txtData";
+            // 
+            // comboEC
+            // 
+            resources.ApplyResources(this.comboEC, "comboEC");
+            this.comboEC.FormattingEnabled = true;
+            this.comboEC.Name = "comboEC";
+            // 
+            // txtTelefone
+            // 
+            resources.ApplyResources(this.txtTelefone, "txtTelefone");
+            this.txtTelefone.Name = "txtTelefone";
+            // 
+            // checkCasa
+            // 
+            resources.ApplyResources(this.checkCasa, "checkCasa");
+            this.checkCasa.Name = "checkCasa";
+            this.checkCasa.UseVisualStyleBackColor = true;
+            // 
+            // checkVeiculo
+            // 
+            resources.ApplyResources(this.checkVeiculo, "checkVeiculo");
+            this.checkVeiculo.Name = "checkVeiculo";
+            this.checkVeiculo.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioO);
+            this.groupBox1.Controls.Add(this.radioF);
+            this.groupBox1.Controls.Add(this.radioM);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // radioM
+            // 
+            resources.ApplyResources(this.radioM, "radioM");
+            this.radioM.Checked = true;
+            this.radioM.Name = "radioM";
+            this.radioM.TabStop = true;
+            this.radioM.UseVisualStyleBackColor = true;
+            // 
+            // radioF
+            // 
+            resources.ApplyResources(this.radioF, "radioF");
+            this.radioF.Name = "radioF";
+            this.radioF.UseVisualStyleBackColor = true;
+            // 
+            // radioO
+            // 
+            resources.ApplyResources(this.radioO, "radioO");
+            this.radioO.Name = "radioO";
+            this.radioO.UseVisualStyleBackColor = true;
+            // 
+            // lista
+            // 
+            resources.ApplyResources(this.lista, "lista");
+            this.lista.FormattingEnabled = true;
+            this.lista.Name = "lista";
+            this.lista.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lista_MouseDoubleClick);
+            // 
+            // btnCadastrar
+            // 
+            resources.ApplyResources(this.btnCadastrar, "btnCadastrar");
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // btnExcluir
+            // 
+            resources.ApplyResources(this.btnExcluir, "btnExcluir");
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnLimpar
+            // 
+            resources.ApplyResources(this.btnLimpar, "btnLimpar");
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkVeiculo);
+            this.Controls.Add(this.checkCasa);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.comboEC);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.DateTimePicker txtData;
+        private System.Windows.Forms.ComboBox comboEC;
+        private System.Windows.Forms.MaskedTextBox txtTelefone;
+        private System.Windows.Forms.CheckBox checkCasa;
+        private System.Windows.Forms.CheckBox checkVeiculo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioF;
+        private System.Windows.Forms.RadioButton radioM;
+        private System.Windows.Forms.RadioButton radioO;
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ SistemaCadastro/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaCadastro
+{
+    public partial class Form1 : Form
+    {
+        // Lista genérica: coleção dinâmica (Lista do tipo Pessoa)
+        // é instanciada na raiz para que seja possível acessá-la em todo o código
+        List<Pessoa> pessoas; // nome da coleção é pessoas
+
+        // Construtor
+        public Form1()
+        {
+            InitializeComponent();
+            pessoas = new List<Pessoa>(); // inicializa a lista 
+
+            // Add opções
+            comboEC.Items.Add("Casado");
+            comboEC.Items.Add("Solteiro");
+            comboEC.Items.Add("Viúvo");
+            comboEC.Items.Add("Separado");
+
+            comboEC.SelectedIndex = 0; // Deixa o elemento 1 do combo selecionado por default
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            int index = -1; // Utilizado para verificar se a pessoa já está cadastrada (-1 = não cadastrada)
+
+            foreach (Pessoa pessoa in pessoas)
+            {
+                // verifica se a pessoa já está cadastrada (Compara os nomes da lista com o valor passado no textBox)
+                if(pessoa.Nome == txtNome.Text)
+                {
+                    index = pessoas.IndexOf(pessoa);
+                    // MessageBox.Show("Usuário já cadastrado!");
+                }
+            }
+
+            // Verifica se os campos foram preenchidos
+            if(txtNome.Text == "")
+            {
+                MessageBox.Show("Preencha o campo nome."); // Exibe caixa com mensagem
+                txtNome.Focus(); // Foca o cursor no campo nome
+                return; // Sai do método e finaliza execução do código (não permite avançar sem o preenchimento)
+            }
+            
+            if(txtTelefone.Text == "(  )      -")
+            {
+                MessageBox.Show("Preencha o campo telefone."); // Exibe caixa com mensagem
+                txtTelefone.Focus(); // Foca o cursor no campo telefone
+                return; // Sai do método e finaliza execução do código (não permite avançar sem o preenchimento)
+            }
+
+
+            char sexo; // Variável para salvar o sexo (um caractere)
+
+            // Verifica qual sexo foi selecionado (char deve ser escrito entre aspas simples)
+            if (radioM.Checked)  
+            { 
+                sexo = 'M';
+            } 
+            else if (radioF.Checked) 
+            { 
+                sexo = 'F';
+            }
+            else { 
+                sexo = 'O'; 
+            }
+
+            // Dados preenchidos -> cria objeto do tipo pessoa com os dados e insere na lista
+            Pessoa p = new Pessoa();
+            p.Nome = txtNome.Text;
+            p.DataNascimento = txtData.Text; // data retorna string
+            p.EstadoCivil = comboEC.SelectedItem.ToString(); // Pega o item que está em um objeto e converte para string
+            p.Telefone = txtTelefone.Text;
+            p.CasaPropria = checkCasa.Checked; // salva true ou false (bool)
+            p.Veiculo = checkVeiculo.Checked;
+            p.Sexo = sexo; // Seta o valor que foi definido na verificação acima.
+
+
+            // Decide se vai cadastrar uma nova pessoa ou atualizar registro
+            if(index < 0)
+            {
+                // -1 = Nâo existe, então cadastra.
+                pessoas.Add(p);
+            }
+            else
+            {
+                // a partir do 0, cadastro existe, então atualiza.
+                pessoas[index] = p;
+            }
+
+
+            // Chama o método para limpar todos os campos
+            btnLimpar_Click(btnLimpar, EventArgs.Empty); // objeto passado é o próprio btnLimpar -> sem info para evento
+
+            // Lista todos os registros
+            Listar();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            // Utilizado para pegar o índice do registro a ser excluído
+            int index = lista.SelectedIndex; // selectedIndex => selecionado ao clicar no registro
+            pessoas.RemoveAt(index); // exclui
+
+            Listar(); // Exibe lista atualizada
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = ""; 
+            txtData.Text = ""; 
+            comboEC.SelectedItem = 0; // elemento 1 selecionado por padrão
+            txtTelefone.Text = "";
+            checkCasa.Checked = false; 
+            checkVeiculo.Checked = false;
+            radioM.Checked = true; // Selecionado por padrão
+            radioF.Checked = false; 
+            radioO.Checked = false; 
+            txtNome.Focus(); // Foca no campo Nome
+        }
+
+        private void Listar()
+        {
+            lista.Items.Clear(); // Acessa o controle e remote todos os registros
+
+            // Percorre a lista e exibe todos os registros
+            foreach (Pessoa p in pessoas)
+            {
+                lista.Items.Add($"{p.Nome} | {p.Telefone}"); // Add pelo nome
+            }
+
+        }
+
+        private void lista_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = lista.SelectedIndex; // Pega o indice do elemento selecionado na lista (controle)
+            Pessoa p = pessoas[index]; // Pega o objeto salvo
+
+            // Passa os valores do objeto para os campos
+            txtNome.Text = p.Nome;
+            txtData.Text = p.DataNascimento;
+            comboEC.SelectedItem = p.EstadoCivil; 
+            txtTelefone.Text = p.Telefone;
+            checkCasa.Checked = p.CasaPropria;
+            checkVeiculo.Checked = p.Veiculo;
+
+            // Verificação para saber o sexo selecionado
+            switch (p.Sexo)
+            {
+                case 'M':
+                    radioM.Checked = true; 
+                    break;
+                case 'F':
+                    radioF.Checked = true;
+                    break;
+                default:
+                   radioO.Checked = true;
+                    break;
+            }
+            txtNome.Focus(); 
+        }
+    }
+}
